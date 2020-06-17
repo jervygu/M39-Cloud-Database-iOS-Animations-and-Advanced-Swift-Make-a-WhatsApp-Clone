2a22c3d89df39f0a7b60397cf41ee8847edb4e55
jervygu <jervygu@gmail.com>
2020-06-17 14:44:02 +0800

    Flash Chat Register, Login and Logout done

--- Flash-Chat-iOS12-master/Flash Chat/RegisterViewController.swift
+++ Flash-Chat-iOS12-master/Flash Chat/RegisterViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -32,6 +32,17 @@ class RegisterViewController: UIViewController {
 
         
         //TODO: Set up a new user on our Firbase database
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                // successful
+                print("Registration Sucessful!")
+                
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
+        
         
         
 
